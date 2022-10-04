7d2e5940b55888a05da3c1bba067df0e9fce3be9
dasilvaca <dasilvaca@unal.edu.co>
2022-10-04 18:48:32 -0500

    Fixed bugs while testing in POSTMAN

--- schedule/views.py
+++ schedule/views.py
@@ -10,61 +10,84 @@ import json
 
 db = connect("actualCourses")
 
+
 @api_view(['GET'])
 def get_every_course(request):
-    items = [x for x in db.find({},{'_id' : 0})] 
-    
+    items = [x for x in db.find({}, {'_id': 0})]
+
     if request.method == 'GET':
         return Response(items)
-    return Response({'error':'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response({'error': 'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
-def get_course_by_name_and_group(request, course_name, group): 
-    items = [x for x in db.find_one({
-        "courseName": course_name, 
+def get_course_by_name_and_group(request, course_name, group):
+    items = [x for x in db.find({
+        "course_name": course_name,
         "group": group},
-        {'_id' : 0}
-        )] 
+        {'_id': 0}
+    )]
 
     if request.method == 'GET':
         return Response(items)
-    return Response({'error':'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response({'error': 'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(['GET'])
 def get_course_by_name(request, course_name):
-    items = [x for x in db.find_one({
-        "courseName": course_name},
-        {'_id' : 0}
-        )] 
+    items = [x for x in db.find({
+        "course_name": course_name},
+        {'_id': 0}
+    )]
 
     if request.method == 'GET':
         return Response(items)
-    return Response({'error':'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response({'error': 'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(['GET'])
 def get_every_course_a_student_is_in(request, student_id):
     items = [x for x in db.find({
-        "students": student_id},
-        {'_id' : 0}
-        )] 
+        "students_in_course.id": student_id},
+        {'_id': 0}
+    )]
 
     if request.method == 'GET':
         return Response(items)
-    return Response({'error':'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response({'error': 'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET'])
+def get_a_course_a_student_is_in_filtered_by_student(request, student_id, course_name):
+    items = [x for x in db.find(
+        {
+            "students_in_course.id": student_id,
+            "course_name": course_name
+        },
+        {"students_in_course": {
+            "$elemMatch": {"id": student_id}}
+         ,"course_name": 1, "_id": 0, "group": 1, "students_in_course": 1}
+    )]
+
+    if request.method == 'GET':
+        return Response(items)
+    return Response({'error': 'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(['GET'])
 def get_every_course_a_student_is_in_filtered_by_student(request, student_id):
     items = [x for x in db.find(
-        { 
-            "students_in_course.id": "1" 
-        }, 
-        { "students_in_course": {
-             "$elemMatch": { "id": "1" } 
-            }, "course_name": 1, "_id": 0, "group": 1 
-        }
-        )] 
+        {
+            "students_in_course.id": student_id
+        },
+        {"students_in_course": {
+            "$elemMatch": {"id": student_id}
+        },
+        "course_name": 1, "_id": 0, "group": 1
+        }#, "students_in_course": 1}
+
+    )]
 
     if request.method == 'GET':
         return Response(items)
-    return Response({'error':'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response({'error': 'Try using the correct REST method'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
